0c743dd9253cfcf29de1056a170304b8a5cd218c
Zeynal <zeynal.zeynalov.00@mail.ru>
2019-10-16 21:44:50 +0300

    time checking was added

--- lab_02/Matrix_Vinograd/main.cpp
+++ lab_02/Matrix_Vinograd/main.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
+#include <time.h>
+#include <windows.h>
+#include <chrono>
 
 #define OK 0
 #define ERR_MEM             -1
 #define ERR_EMPTY_MATRIX    -2
 #define ERR_MTR_SIZE        -3
 using namespace std;
+unsigned long long tick(void)
+{
+    unsigned long long d;
+    __asm__ __volatile__ ("rdtsc" : "=A" (d) );
+
+    return d;
+}
 
 int** allocateMatrix(int n, int m)
 {
@@ -19,14 +29,12 @@ int** allocateMatrix(int n, int m)
     }
     return matrix;
 }
-
 void clearMatrix(int **matrix, int row_count)
 {
     for (int i = 0; i < row_count; i++)
         delete matrix[i];
     delete matrix;
 }
-
 
 
 void printMatrix(int **matrix, int row_count, int column_count)
@@ -41,7 +49,6 @@ void printMatrix(int **matrix, int row_count, int column_count)
         cout << endl;
     }
 }
-
 int fillMatrix(int **matrix, int rowCount, int columnCount)
 {
     if (matrix == NULL)
@@ -62,7 +69,6 @@ int fillMatrix(int **matrix, int rowCount, int columnCount)
     return OK;
 }
 
-
 int createMatrix(int ***matrix, int rowCount, int columnCount)
 {
     int code_error = OK;
@@ -78,7 +84,7 @@ int createMatrix(int ***matrix, int rowCount, int columnCount)
     }
     return code_error;
 }
-
+#pragma optimize("", off)
 int multiplicateMatrix(int ***result, int &rc, int &cc, int** src1, int **src2, int rc1, int cc1, int rc2, int cc2)
 {
     if (cc1 != rc2)
@@ -104,7 +110,7 @@ int multiplicateMatrix(int ***result, int &rc, int &cc, int** src1, int **src2, 
     }
     *result = tmpMtr;
 }
-
+#pragma optimize("", off)
 int multiplicateMatrixVinograd(int ***result, int &rc, int &cc, int** src1, int **src2, int rc1, int cc1, int rc2, int cc2)
 {
     if (cc1 != rc2)
@@ -126,7 +132,7 @@ int multiplicateMatrixVinograd(int ***result, int &rc, int &cc, int** src1, int 
         {
 
             MulH[i] = MulH[i] + src1[i][j * 2] * src1[i][2 * j + 1];
-            cout << MulH[i] << endl;
+            //cout << MulH[i] << endl;
         }
     }
     for (int i = 0; i < cc2; i++)
@@ -134,7 +140,7 @@ int multiplicateMatrixVinograd(int ***result, int &rc, int &cc, int** src1, int 
         for (int j = 0; j < rc2 / 2; j++)
         {
             MulV[i] = MulV[i] + src2[j * 2][i] * src2[2 * j + 1][i];
-            cout << src2[j * 2][i] * src2[2 * j + 1][i] << endl;
+            //cout << src2[j * 2][i] * src2[2 * j + 1][i] << endl;
         }
     }
     for (int i = 0; i < rc1; i++)
@@ -162,7 +168,7 @@ int multiplicateMatrixVinograd(int ***result, int &rc, int &cc, int** src1, int 
     }
     *result = tmpMtr;
 }
-
+#pragma optimize("", off)
 int optimizedMultiplication(int ***result, int &rc, int &cc, int** src1, int **src2, int rc1, int cc1, int rc2, int cc2)
 {
 
@@ -182,19 +188,19 @@ int optimizedMultiplication(int ***result, int &rc, int &cc, int** src1, int **s
     int *MulV = (int*)calloc(cc2, sizeof(int));
     for (int i = 0; i < rc1; i++)
     {
-        for (int j = 0; j < rc2 / 2; j++)
+        for (int j = 0; j < rc2_2; j++)
         {
 
             MulH[i] = MulH[i] + src1[i][j * 2] * src1[i][2 * j + 1];
-            cout << MulH[i] << endl;
+            //cout << MulH[i] << endl;
         }
     }
     for (int i = 0; i < cc2; i++)
     {
-        for (int j = 0; j < rc2 / 2; j++)
+        for (int j = 0; j < rc2_2; j++)
         {
             MulV[i] = MulV[i] + src2[j * 2][i] * src2[2 * j + 1][i];
-            cout << src2[j * 2][i] * src2[2 * j + 1][i] << endl;
+            //cout << src2[j * 2][i] * src2[2 * j + 1][i] << endl;
         }
     }
     int N = rc2 - 1;
@@ -225,36 +231,80 @@ int optimizedMultiplication(int ***result, int &rc, int &cc, int** src1, int **s
     *result = tmpMtr;
 }
 
-
 int main()
 {
+    int start = 100;
+    int end = 900;
+    int step = 100;
+    int rep = 10;
+
+    unsigned long long int tBegin = 0, tEnd = 0;
+    double tSum = 0;
     int **matrix1 = NULL;
     int **matrix2 = NULL;
     int **result = NULL;
     int res_r = 0, res_c = 0;
     int code_error = OK;
-    int rowCount1 = 2;
-    int columnCount1 = 2;
-    int rowCount2 = 2;
-    int columnCount2 = 3;
-    code_error = createMatrix(&matrix1, rowCount1, columnCount1);
-    if (code_error == OK)
+
+
+    //printMatrix(matrix1, rowCount1, columnCount1);
+    //printMatrix(matrix2, rowCount2, columnCount2);
+    //multiplicateMatrix(&result, res_r, res_c, matrix1, matrix2, rowCount1, columnCount1, rowCount2, columnCount2);
+    //multiplicateMatrixVinograd(&result, res_r, res_c, matrix1, matrix2, rowCount1, columnCount1, rowCount2, columnCount2);
+    //optimizedMultiplication(&result, res_r, res_c, matrix1, matrix2, rowCount1, columnCount1, rowCount2, columnCount2);
+    //cout << endl;
+    //printMatrix(result, res_r, res_c);
+    for (int i = start; i < end; i += step)
     {
-        code_error = createMatrix(&matrix2, rowCount2, columnCount2);
+        cout << endl;
+        cout << "===== for " << i << " size ========" << endl;
+        cout << endl;
+        code_error = createMatrix(&matrix1, i, i);
         if (code_error == OK)
         {
-            printMatrix(matrix1, rowCount1, columnCount1);
-            printMatrix(matrix2, rowCount2, columnCount2);
-            //multiplicateMatrix(&result, res_r, res_c, matrix1, matrix2, rowCount1, columnCount1, rowCount2, columnCount2);
-            //multiplicateMatrixVinograd(&result, res_r, res_c, matrix1, matrix2, rowCount1, columnCount1, rowCount2, columnCount2);
-            optimizedMultiplication(&result, res_r, res_c, matrix1, matrix2, rowCount1, columnCount1, rowCount2, columnCount2);
-            cout << endl;
-            printMatrix(result, res_r, res_c);
+            code_error = createMatrix(&matrix2, i, i);
+            if (code_error == OK)
+            {
+                tSum = 0;
+                for (int j = 0; j < rep; j++)
+                {
+                    const auto start = std::chrono::system_clock::now();
+                    multiplicateMatrix(&result, res_r, res_c, matrix1, matrix2, i, i, i, i);
+                    const auto end = std::chrono::system_clock::now();
+                    const auto res = static_cast<double>(std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count()) / 1.0e9;
+                    tSum += res;
+                }
+                tSum /= rep;
+
+                cout << "time simple Matrix is " << tSum << endl;
+
+                tSum = 0;
+                for (int j = 0; j < rep; j++)
+                {
+                    const auto start = std::chrono::system_clock::now();
+                    multiplicateMatrixVinograd(&result, res_r, res_c, matrix1, matrix2, i, i, i, i);
+                    const auto end = std::chrono::system_clock::now();
+                    const auto res = static_cast<double>(std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count()) / 1.0e9;
+                    tSum += res;
+                }
+                //cout << tSum << " ==== " << tBegin << " ====B=== " << tEnd << " ===E==== " << endl;
+                tSum /= rep;
+                cout << "time Matrix Vinograd is " << tSum << endl;
+
+                tSum = 0;
+                for (int j = 0; j < rep; j++)
+                {
+                    const auto start = std::chrono::system_clock::now();
+                    optimizedMultiplication(&result, res_r, res_c, matrix1, matrix2, i, i, i, i);
+                    const auto end = std::chrono::system_clock::now();
+                    const auto res = static_cast<double>(std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count()) / 1.0e9;
+                    tSum += res;
+                }
+                tSum /= rep;
+                cout << "time Matrix Vinograd Optimized is " << tSum << endl;
+
+            }
         }
-    }
-    else
-    {
-        cout << "something goes wrong " << code_error <<  endl;
     }
     return 0;
 }
